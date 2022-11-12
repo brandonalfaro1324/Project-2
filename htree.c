673a1a28a3932ae869f30643e8e277546331b14f
Brandon Alfaro <brandonlfr@MacBook-Air.local>
2022-11-11 18:16:43 -0600

    Thread tree is working, needs more testing

--- htree.c
+++ htree.c
@@ -8,32 +8,28 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <pthread.h>
 
 #define BSIZE 4096
 
-struct nblock_and_thread {   
-  uint32_t nblocks;          
-  uint32_t nblocks_each_thread;   
-  uint8_t threads_block_extra;          
-  uint8_t threads;          
-}; 
-
 struct recursive_variables {   
-  //uint32_t nblocks;          
-  uint32_t nblocks_for_each_thread;   
-  uint8_t threads_with_1more;          
-  uint8_t threads;    
-  uint8_t thread_id;      
+  //uint32_t nblocks;
+  uint8_t whole_file;
+  struct stat file_data;          
+  uint32_t total_nblocks_for_each_thread;   
+  uint16_t thread_limit_before_extra_nblocks;      
+  uint16_t threads;
+  uint16_t thread_current_count;      
 }; 
-
 
 // Print out the usage of the program and exit.
 void Usage(char*);
-void thread_testing(uint8_t *, struct recursive_variables);
-
-
+void *thread_testing(/*uint8_t *,*/ void* struct_data);
 
 uint32_t jenkins_one_at_a_time_hash(uint8_t* , uint64_t );
+
+
+
 
 int main(int argc, char** argv) {
   //////////////////////////////////////////////////////////
@@ -42,10 +38,10 @@ int main(int argc, char** argv) {
   uint32_t nblocks;
 
   uint8_t *file_string;
-  uint64_t x = 0;
+  //uint64_t x = 0;
 
-  uint32_t nblocks_each_thread = 0;
-  uint16_t threads_block_extra = 0;
+  uint32_t total_nblocks_for_each_thread = 0;
+  uint16_t add_extra_nblock_thread = 0;
 
   //////////////////////////////////////////////////////////
 
@@ -80,18 +76,17 @@ int main(int argc, char** argv) {
   }
 
 
-
 //////////////////////////////////////////////////////////////////////////
 // Begin seperating nblocks to number of threads
-  uint8_t threads = atoi(argv[2]);
+  uint16_t threads = atoi(argv[2]);
   if(nblocks < threads ||  threads <= 0){
     perror("Number of threads are more than number of blocks...");
     exit(EXIT_FAILURE);
   }
   
   /* IGNORE THIS CODE, THIS CE IS TO SEE IF "nblock/threads" IS WORKING!!!
-  nblocks_each_thread = nblocks/threads;
-  threads_block_extra = nblocks%threads;
+  total_nblocks_for_each_thread = nblocks/threads;
+  add_extra_nblock_thread = nblocks%threads;
   
   uint32_t block_visualization[threads];
 
@@ -99,15 +94,15 @@ int main(int argc, char** argv) {
 
   printf("\nNblocks:  %d", nblocks);
   printf("\nThreads needed:  %d", threads);
-  printf("\nNblocks per thread %d", nblocks_each_thread);    
-  printf("\nThreads that need 1 extra nblock %d\n", threads_block_extra);   
+  printf("\nNblocks per thread %d", total_nblocks_for_each_thread);    
+  printf("\nThreads that need 1 extra nblock %d\n", add_extra_nblock_thread);   
 
 
   for(int i = 0; i < threads; i++){
-    block_visualization[i] = nblocks_each_thread;
+    block_visualization[i] = total_nblocks_for_each_thread;
 
-      if(i > (threads - threads_block_extra - 1)){
-        block_visualization[i] = nblocks_each_thread + 1;
+      if(i > (threads - add_extra_nblock_thread - 1)){
+        block_visualization[i] = total_nblocks_for_each_thread + 1;
       }
   }
   for(int i = 0; i < threads; i++){
@@ -117,17 +112,23 @@ int main(int argc, char** argv) {
 //////////////////////////////////////////////////////////////////////////
 
 
+  total_nblocks_for_each_thread = nblocks/threads;
+  add_extra_nblock_thread = nblocks%threads;
 
+  struct recursive_variables temp_hold = {
+  *file_string,
+  fileStat,
+  total_nblocks_for_each_thread, 
+  (threads - add_extra_nblock_thread), 
+  threads, 
+  0};  
 
-  nblocks_each_thread = nblocks/threads;
-  threads_block_extra = nblocks%threads;
-  struct recursive_variables temp_struct_hold = {nblocks_each_thread, 
-                                                          threads - threads_block_extra, 
-                                                          threads, 
-                                                          0};  
+  printf("\n%p\n", &temp_hold);
 
-  
-  thread_testing(file_string, temp_struct_hold);
+  pthread_t p1; 
+
+  pthread_create(&p1, NULL, thread_testing, (void *) &temp_hold); 
+  pthread_join(p1, NULL);  
 
 
 
@@ -155,51 +156,83 @@ int main(int argc, char** argv) {
   return EXIT_SUCCESS;
 }
 
+//////////////////////////////////////////////////////////////////////
+void *thread_testing(/*uint8_t *file_data,*/ void* struct_data){
+  
+  struct recursive_variables *temp = malloc(sizeof(struct recursive_variables));
+  temp = struct_data;
+  int currrent_thread = temp->thread_current_count;
+
+  if(currrent_thread < temp->threads){
+    if(currrent_thread < temp->thread_limit_before_extra_nblocks){
+      printf("\n[%d : %d]", currrent_thread, temp->total_nblocks_for_each_thread);
+    }
+    else{
+      printf("\n[%d : %d]", currrent_thread, temp->total_nblocks_for_each_thread + 1);
+    }
+
+    struct recursive_variables temp1 = {
+      temp->whole_file,
+      temp->file_data,          
+      temp->total_nblocks_for_each_thread,   
+      temp->thread_limit_before_extra_nblocks,      
+      temp->threads,
+      (2 * temp->thread_current_count + 1)   
+    };    
+
+
+    struct recursive_variables temp2 = {
+      temp->whole_file,
+      temp->file_data,          
+      temp->total_nblocks_for_each_thread,   
+      temp->thread_limit_before_extra_nblocks,      
+      temp->threads,
+      (2 * temp->thread_current_count + 2)   
+    };    
+
+    pthread_t p1, p2;
+    pthread_create(&p1, NULL, thread_testing, (void *) &temp1); 
+    pthread_create(&p2, NULL, thread_testing, (void *) &temp2); 
+
+    pthread_join(p1, NULL);  
+    pthread_join(p2, NULL);  
+  }
+  return NULL;
+}
+
 
 //////////////////////////////////////////////////////////////////////
+
+
+
+
+/*
 void thread_testing(uint8_t *file_data, struct recursive_variables struct_data){
   
-
-  //int thread_minus_one = struct_data.threads - struct_data.threads_with_1more - 1;
-
   int one_or_two = 0;
-  int currrent_thread = struct_data.thread_id;
+  int currrent_thread = struct_data.thread_current_count;
 
   if(currrent_thread < struct_data.threads){
     //printf("\n[current thread=%d : thread=%d]\n", currrent_thread, struct_data.threads);
     //printf("PASS\n");
     //printf("\nThread number: %d", currrent_thread);
-    if(currrent_thread < struct_data.threads_with_1more){
-      printf("\n[%d : %d]", currrent_thread, struct_data.nblocks_for_each_thread);
-      //printf("\n NO EXTRA: [%d]\n", currrent_thread);
-
+    if(currrent_thread < struct_data.threads){
+      printf("\n[%d : %d]", currrent_thread, struct_data.total_nblocks_for_each_thread);
     }
     else{
-      printf("\n[%d : %d]", currrent_thread, struct_data.nblocks_for_each_thread + 1);
-      //printf("\n Threads that need one extra: [%d]\n", currrent_thread);
+      printf("\n[%d : %d]", currrent_thread, struct_data.total_nblocks_for_each_thread + 1);
     }
 
-      struct_data.thread_id = 2 * currrent_thread + 1;
+      struct_data.thread_current_count = 2 * currrent_thread + 1;
       thread_testing(file_data, struct_data);
 
-      struct_data.thread_id = 2 * currrent_thread + 2;
-      thread_testing(file_data, struct_data);
-  
-  }  
-
-
+      struct_data.thread_current_count = 2 * currrent_thread + 2;
+      thread_testing(file_data, struct_data);  
+    }
 }
-//////////////////////////////////////////////////////////////////////
+  */
 
-/*
-struct recursive_variables {   
-  //uint32_t nblocks;          
-  uint32_t nblocks_for_each_thread;   
-  uint16_t threads_with_1more;          
-  uint16_t threads;    
-  uint16_t thread_id;      
-}; 
-*/
+
 
 
 
