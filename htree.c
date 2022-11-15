cbacb179b01a35ea0056c0c8847a8c5f0e3a1bb4
Brandon Alfaro <brandonlfr@MacBook-Air.local>
2022-11-15 15:42:51 -0600

    Added several edits

--- htree.c
+++ htree.c
@@ -14,9 +14,6 @@
 #define BSIZE 4096
 
 struct recursive_variables {   
-  //uint32_t nblocks;
-
-  struct stat file_data;          
   uint16_t threads;
   uint32_t nblocks_each_thread;   
   uint16_t thread_current_count;
@@ -31,17 +28,16 @@ int number_of_integers(uint64_t);
 
 
 uint8_t *file_string;
-struct stat fileStat;
-
 
 int main(int argc, char** argv) {
-
 
   //////////////////////////////////////////////////////////
   int32_t fd;
   uint32_t nblocks;
 
   uint32_t nblocks_each_thread = 0;
+  struct stat fileStat;
+
   //////////////////////////////////////////////////////////
 
   // input checking 
@@ -89,7 +85,6 @@ int main(int argc, char** argv) {
   printf("\nnblocks for each thread: %d \n", nblocks_each_thread);
 
   struct recursive_variables *temp_hold = malloc(sizeof(struct recursive_variables));
-  temp_hold->file_data = fileStat;
   temp_hold->threads = threads;
   temp_hold->nblocks_each_thread = nblocks_each_thread;
   temp_hold->thread_current_count = 0;
@@ -194,152 +189,170 @@ int main(int argc, char** argv) {
 
 
 
-void *thread_testing(void* struct_data){
-  struct recursive_variables *thread_node = (struct recursive_variables*)struct_data;
-  int currrent_thread = thread_node->thread_current_count;
-  
-  uint64_t parent_value = 0;
-  if(currrent_thread < thread_node->threads){
 
+
+
+
+
+
+
+void *thread_testing(void* struct_data){
+
+
+  // Set and create "struct_data" and assign it to "*thread_node" to hold
+  struct recursive_variables *thread_node = (struct recursive_variables*)struct_data;
+  // Assing the current thread num, to "currrent_thread" 
+  int currrent_thread = thread_node->thread_current_count;
+
+  // "nblocks_each_thread" will hold num of nblocks each thread must have
+  int nblocks_each_thread = thread_node->nblocks_each_thread;
+  
+
+  
+  uint64_t parent_hash = 0;
+
+  // Check if "currrent_thread" is still valid
+  // If we reach our thread max, skip all this and 
+  // return a "parent_hash", which is just zero
+  if(currrent_thread < thread_node->threads){   
+
+    // Begin by computing current hash value, afterwards
+    // get data from left and right child and hash everything 
+
+    // Get beginning and ending index according to the current thread
+    uint64_t beginning_index = (BSIZE * nblocks_each_thread * currrent_thread);
+    uint64_t ending_index = (BSIZE * ((currrent_thread + 1) * nblocks_each_thread));
+
+    // Allocated char variable "holdcharacters[]" to hold the  
+    // specific parts of the main file "file_string"
+
+    // Intialize a counter variable for "holdcharacters[count_i]"
+    // Since we need to store the first element to the end
+    uint64_t count_i = 0;
+    uint8_t *holdcharacters = (uint8_t *) malloc ((nblocks_each_thread * BSIZE) *sizeof(uint8_t));
+    for(int i = beginning_index; i < ending_index; i++){
+      holdcharacters[count_i++] = file_string[i];
+    }
+
+
+    parent_hash = jenkins_one_at_a_time_hash(holdcharacters, (BSIZE * nblocks_each_thread));
+  
+
+
+
+
+
+
+
+
+
+    // Allocate 2 "struct recursive_variables" variables to pass to "pthread_create"
     struct recursive_variables *temp1 = malloc(sizeof(struct recursive_variables));
-      temp1->file_data = thread_node->file_data;
-      temp1->nblocks_each_thread = thread_node->nblocks_each_thread;
+      temp1->nblocks_each_thread = nblocks_each_thread;
       temp1->threads = thread_node->threads;
-      temp1->thread_current_count = (2 * thread_node->thread_current_count + 1);   
+      temp1->thread_current_count = (2 * currrent_thread + 1);   
 
     struct recursive_variables *temp2 = malloc(sizeof(struct recursive_variables));
-      temp2->file_data = thread_node->file_data;
-      temp2->nblocks_each_thread = thread_node->nblocks_each_thread;
+      temp2->nblocks_each_thread = nblocks_each_thread;
       temp2->threads = thread_node->threads;
-      temp2->thread_current_count = (2 * thread_node->thread_current_count + 2);   
+      temp2->thread_current_count = (2 * currrent_thread + 2);   
 
 
-    uint64_t left_child;
-    uint64_t right_child;
+    // Create two variables to hold returning data from threads
+    uint64_t left_child_hash;
+    uint64_t right_child_hash;
     
+    // Intialize the threads and pass in previous allocated struct variables 
     pthread_t p1, p2;
 
     pthread_create(&p1, NULL, thread_testing, temp1); 
     pthread_create(&p2, NULL, thread_testing, temp2); 
-    pthread_join(p1, (void *) &left_child);  
-    pthread_join(p2, (void *) &right_child);  
+    pthread_join(p1, (void *) &left_child_hash);  
+    pthread_join(p2, (void *) &right_child_hash);  
     
+    // Free the previous struct variables when coming back from pthreads
+    free(temp1); 
+    free(temp2); 
 
 
 
 
 
 
+    int total_count = 0;
 
-      uint64_t beginning = (BSIZE * thread_node->nblocks_each_thread * thread_node->thread_current_count);
-      uint64_t ending = (BSIZE * ((thread_node->thread_current_count + 1) * thread_node->nblocks_each_thread));
+    if(left_child_hash == 0 && right_child_hash == 0){
 
-      uint64_t x = 0;
-      uint8_t *holdcharacters = (uint8_t *) malloc ((thread_node->nblocks_each_thread * BSIZE) *sizeof(uint8_t));
-      //printf("\nTEST1\n");
-      //printf("\nStarting point: %d, Ending point: %d", beginning/BSIZE, ending/BSIZE);
-      //printf("\nStarting point CHAR: %c, Ending point CHAR: %c\n", file_string[beginning], file_string[ending - 1]);
-      
-
-
-
-
-
-
-
-
-      uint64_t count_i = 0;
-      for(int i = beginning; i < ending; i++){
-        holdcharacters[count_i++] = file_string[i];
-      }
-      //printf("\nNode %d: count: %d\n", currrent_thread, x / BSIZE);
-      //printf("\nNODE: %d Beginning and end index [%d : %d]", thread_node->thread_current_count, beginning, ending);
-      //printf("\n%d\n", thread_node->nblocks_each_thread * BSIZE);
-      parent_value = jenkins_one_at_a_time_hash(holdcharacters, (BSIZE * thread_node->nblocks_each_thread));
-    
-      if(left_child == 0 && right_child == 0){
-        return (void *) parent_value;
-      }
-
-      int parent_integer = number_of_integers(parent_value);
-      int left_integer = number_of_integers(left_child);
-      int right_integer = number_of_integers(right_child);
-      int total_count = 0;
-    /*
-      printf("\n\nCURRENT THREAD: %d\n", currrent_thread);
-      printf("\nPARENT VALUE: %lld, AND INTEGER: %d", parent_value, parent_integer-1);
-      printf("\nLEFT CHILD VALIE: %lld, AND INTEGER: %d", left_child, left_integer-1);
-      printf("\nLEFT CHILD VALIE: %lld, AND INTEGER: %d", right_child, right_integer-1);
-    */
-      char parent_string[parent_integer+1];
-      char l_child_string[left_integer+1];
-      char r_child_string[right_integer+1];
-
-      sprintf(parent_string, "%lld", parent_value);
-      sprintf(l_child_string, "%lld", left_child);
-      sprintf(r_child_string, "%lld", right_child);
-    /*
-      printf("\nPARENT: %s, LEFT CHILD: %s, RIGHT CHILD: %s", parent_string, l_child_string, r_child_string);
-    */
-
-
-
-
-
-
-
-
-
-
-
-
-      char first_concat[100];
-
-      if(left_integer != 0 && right_integer != 0){
-        printf("\n1 left_integer != 0 && right_integer != 0");
-        strcat(first_concat,parent_string);
-        strcat(first_concat,l_child_string);
-        strcat(first_concat,r_child_string);
-        total_count = parent_integer + left_integer + right_integer;
-      }
-      else{
-        if(left_integer == 0 && right_integer != 0){
-        printf("\n2 left_integer == 0 && right_integer != 0");
-          strcat(first_concat,parent_string);
-          strcat(first_concat,r_child_string);
-          total_count = parent_integer + right_integer;
-        }
-        else if(left_integer != 0 && right_integer == 0){
-        printf("\n3 left_integer != 0 && right_integer == 0");
-          strcat(first_concat,parent_string);
-          strcat(first_concat,l_child_string);
-          total_count = parent_integer + left_integer;
-        }
-        else{
-        printf("\n4 JUST PARENT");
-          strcat(first_concat,parent_string);
-          total_count = parent_integer;
-        }
-      }
+      /*  WILL NOT WORK!!!
       printf("\ncurrent Thread: %d", currrent_thread);
       printf("\nConcatenated String: %s, AND Conca. value: %d", first_concat, total_count);
-      parent_value = jenkins_one_at_a_time_hash(first_concat, total_count);
-      printf("\nConcatenated String: %lld", parent_value);
-      printf("\n\n");
+      printf("\nConcatenated String: %lld", parent_hash);
+      */
+      printf("\nConcatenated String: %lld", parent_hash);
+      return (void *) parent_hash;
+    }
+
+    int parent_integer = number_of_integers(parent_hash);
+    int left_integer = number_of_integers(left_child_hash);
+    int right_integer = number_of_integers(right_child_hash);
+
+
+    char parent_string[parent_integer+1];
+    char l_child_string[left_integer+1];
+    char r_child_string[right_integer+1];
+
+
+    sprintf(parent_string, "%lld", parent_hash);
+    sprintf(l_child_string, "%lld", left_child_hash);
+    sprintf(r_child_string, "%lld", right_child_hash);
+   
+
+
+
+
+
+    char first_concat[100];
+
+    if(left_integer != 0 && right_integer != 0){
+      printf("\n1 left_integer != 0 && right_integer != 0");
+      strcat(first_concat,parent_string);
+      strcat(first_concat,l_child_string);
+      strcat(first_concat,r_child_string);
+      total_count = parent_integer + left_integer + right_integer;
+    }
+    else{
+      if(left_integer == 0 && right_integer != 0){
+      printf("\n2 left_integer == 0 && right_integer != 0");
+        strcat(first_concat,parent_string);
+        strcat(first_concat,r_child_string);
+        total_count = parent_integer + right_integer;
+      }
+      else if(left_integer != 0 && right_integer == 0){
+      printf("\n3 left_integer != 0 && right_integer == 0");
+        strcat(first_concat,parent_string);
+        strcat(first_concat,l_child_string);
+        total_count = parent_integer + left_integer;
+      }
+      else{
+      printf("\n4 JUST PARENT");
+        strcat(first_concat,parent_string);
+        total_count = parent_integer;
+      }
+      }
+
       free(holdcharacters);
-      free(temp1); 
-      free(temp2); 
 
 
+      printf("\ncurrent Thread: %d", currrent_thread);
+      printf("\nConcatenated String: %s, AND Conca. value: %d", first_concat, total_count);
+      parent_hash = jenkins_one_at_a_time_hash(first_concat, total_count);
+      printf("\nConcatenated String: %lld", parent_hash);
+      printf("\n\n");
 
-
-
-
-
-
+  
   }
 
-  return (void *) parent_value;
+  return (void *) parent_hash;
 }
 //////////////////////////////////////////////////////////////////////
 
@@ -356,54 +369,13 @@ void *thread_testing(void* struct_data){
 int number_of_integers(uint64_t integer){
     int number_of_integers = 0;
     uint64_t val = integer;
+    
     while(val > 0){      
       val /= 10;
       number_of_integers++;
     }
-
   return number_of_integers;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 uint32_t jenkins_one_at_a_time_hash(uint8_t* key, uint64_t length) {
   uint64_t i = 0;
